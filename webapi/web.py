cc2b30380aa88b641ff86cd141ed75460f4d9e04
beimingfishing <624143644@qq.com>
2024-06-27 16:18:27 +0800

    faces dect and draw rect

--- webapi/web.py
+++ webapi/web.py
@@ -18,38 +18,71 @@ class Params(BaseModel):
 async def read_root():
     return {"Hello": "World"}
 
+
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
+
 @app.post("/upload_pic")
 async def upload_pic_from_user(file: UploadFile = File(...),
-                               params:  Params = Form(...)
+                               params: Params = Form(...)
                                ):
     try:
         # 读取上传的文件内容
         image_bytes = await file.read()
 
-        # 使用 BytesIO 来创建一个可读的对象
-        image_stream = BytesIO(image_bytes)
-
         # 使用 OpenCV 读取图像
         image = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), cv2.IMREAD_COLOR)
-
-        if image is not None:
-            faces = DeepFaceDect.get_all_face_in_pic(image)
+        # 将bgr转化为rgb
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        if image_rgb is not None:
+            faces = DeepFaceDect.get_all_face_in_pic(image_rgb)
+        if faces is not None:
             if len(faces) >= 2:
                 return {
-                    "message" : "picture has two face"
+                    "message": "picture has two face"
                 }
-        if faces is not None:
-            DeepFaceDect.upload_face_from_user(image, params.user_name, params.face_in_name)
+            else:
+                DeepFaceDect.upload_face_from_user(image_rgb, params.user_name, params.face_in_name)
 
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
+
 
 @app.post("/facedect/facesDect")
 async def dect_all_face(file: UploadFile = File(...),
                         params: Params = Form(...)
                         ):
     try:
+        image_bytes = await file.read()
+        image_bgr = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), cv2.IMREAD_COLOR)
+        image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
+
+        faces = DeepFaceDect.get_all_face_in_pic(image_rgb)
+        if faces is not None:
+            name_of_pic = []
+            for face in faces:
+                compare_result = DeepFaceDect.compare_and_verify_face(face['face'], params.user_name)
+                point_at_rightDown=(face['facial_area']['x'] + face['facial_area']['w'], face['facial_area']['y'] + face['facial_area']['h'])
+                cv2.rectangle(image_rgb, (face['facial_area']['x'], face['facial_area']['y']),
+                              point_at_rightDown,
+                              (0, 0, 255),
+                              2)
+                if compare_result != 'not found match face':
+                    cv2.putText(image_rgb,
+                                compare_result,
+                                (face['facial_area']['x'], face['facial_area']['y']),
+                                cv2.FONT_HERSHEY_PLAIN,
+                                0.7,
+                                (0, 0, 255)
+                                )
+
+        else:
+            return {
+                "message": "no face"
+            }
+
+    except Exception as e:
+        # 如果发生错误，返回错误信息
+        raise HTTPException(status_code=500, detail=str(e))
